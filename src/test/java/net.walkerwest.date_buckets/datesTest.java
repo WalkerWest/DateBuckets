595d6960589b01edc268687dcebff3173a5fa785
root <Walker West>
2022-11-16 06:00:21 -0600

    Added one more unit test and set the stage for a third.

--- src/test/java/net.walkerwest.date_buckets/datesTest.java
+++ src/test/java/net.walkerwest.date_buckets/datesTest.java
@@ -13,7 +13,31 @@ public class datesTest {
 		ZonedDateTime beg = ZonedDateTime.
 			of(2022, 11, 14, 06, 0, 0, 0000, zoneId);
 		ZonedDateTime end = ZonedDateTime.
-			of(2022, 11, 15, 17, 59, 59, 0000, zoneId);
+			of(2022, 11, 15, 06, 0, 0, 0001, zoneId);
+		Dates myDates = new Dates();
+		try { Integer[] buckets = myDates.getTimeBuckets(beg,end); } 
+		catch (Dates.GreaterThan24HrsException ex) { throw ex; }
+	}
+
+	@Test
+	public void exactly24HrsTest() throws Dates.GreaterThan24HrsException {
+		ZoneId zoneId = ZoneId.of("UTC");
+		ZonedDateTime beg = ZonedDateTime.
+			of(2022, 11, 14, 06, 0, 0, 0000, zoneId);
+		ZonedDateTime end = ZonedDateTime.
+			of(2022, 11, 15, 06, 0, 0, 0000, zoneId);
+		Dates myDates = new Dates();
+		try { Integer[] buckets = myDates.getTimeBuckets(beg,end); } 
+		catch (Dates.GreaterThan24HrsException ex) { throw ex; }
+	}
+
+	@Test
+	public void dstTest() throws Dates.GreaterThan24HrsException {
+		ZoneId zoneId = ZoneId.of("UTC");
+		ZonedDateTime beg = ZonedDateTime.
+			of(2022, 11, 05, 02, 0, 0, 0000, zoneId);
+		ZonedDateTime end = ZonedDateTime.
+			of(2022, 11, 06, 02, 0, 0, 0000, zoneId);
 		Dates myDates = new Dates();
 		try { Integer[] buckets = myDates.getTimeBuckets(beg,end); } 
 		catch (Dates.GreaterThan24HrsException ex) { throw ex; }
